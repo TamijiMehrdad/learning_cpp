ffacff7c634b418c1624417e7218c4e9367208da
mt <mehrdad.tamiji@gmail.com>
2020-12-25 20:02:33 +0330

    first commit for quizes of learningcpp.com

---
+++ test.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iterator> 
+#include <algorithm>
+#include <limits>
+#include <random>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+#include <typeinfo>
+#include <array>
+#include <vector>
+#include <string_view>
+#include <numeric> // std::reduce
+
+
+enum class Items
+{
+	health_potions,
+	torches,
+	arrows
+};
+
+int countTotalItems(const std::array<int,3> &arr)
+{
+	return std::reduce(arr.begin(), arr.end());
+
+}
+int main()
+{
+	std::array player{2,5,10};
+    return 0;
+}
