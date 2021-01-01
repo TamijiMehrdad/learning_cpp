d86d027027db088840494110e5936ead2a275e2b
mt <mehrdad.tamiji@gmail.com>
2021-01-01 18:29:19 +0330

    quiz of 10.5

---
+++ chapter_10/10_5.cpp
@@ -0,0 +1,32 @@
+#include "cpp_tools.h"
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <limits>
+#include <random>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+#include <typeinfo>
+#include <array>
+#include <vector>
+#include <string_view>
+#include <numeric>
+#include <cassert>
+#include <tuple>
+ 
+
+int sumTo(const int a);
+
+void printEmployeeName(const Employee &employee);
+
+std::pair<int, int> minmax(const int a, const int b);
+
+int getIndexOfLargestValue(const std::vector<int> &vector);
+
+const std::string& getElement(const std::vector<std::string> &arr,const int index);
+
+int main()
+{
+    return 0;
+}
