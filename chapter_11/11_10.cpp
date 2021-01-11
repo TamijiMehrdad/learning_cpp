e0ae12d55ba275f96fb8f133e7c372b52ae928b9
mt <mehrdad.tamiji@gmail.com>
2021-01-11 09:58:58 +0330

    chapter 11.10 has finished

--- chapter_11/11_10.cpp
+++ chapter_11/11_10.cpp
@@ -19,3 +19,23 @@
 #include <cstdarg>
 #include <functional>
 
+class Clac
+{
+private:
+    int m_result{0};
+public:
+    Clac():m_result{0}
+    {}
+    Clac &add(int value) {m_result += value; return *this;}
+    Clac &sub(int value) {m_result -= value; return *this;}
+    Clac &multi(int value) {m_result *= value; return *this;}
+    Clac &div(int value) {m_result /= value; return *this;}
+    int get_val(){return m_result;}
+};
+
+int main()
+{
+    Clac a{};
+    std::cout << (a.add(5).multi(4).sub(2).div(3).get_val()) << '\n'; //6
+    return 0;
+}
