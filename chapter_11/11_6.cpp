0d32d46205adaa7dee2d46147ace93fb74fbaa7d
mt <mehrdad.tamiji@gmail.com>
2021-01-11 12:13:26 +0330

    files 11_6.cpp and 11_6_quiz have merged. files 11_7.cpp and 11_7_quiz have merged

--- chapter_11/11_6.cpp
+++ chapter_11/11_6.cpp
@@ -36,9 +36,46 @@ public:
 
 };
 
-int main()
+class RGBA
+{
+    using comp_type = std::uint_fast8_t; 
+private:
+    comp_type m_red;
+    comp_type m_green;
+    comp_type m_blue;
+    comp_type m_alpha;
+public:
+    RGBA(comp_type red=0, comp_type green=0, comp_type blue=0, comp_type alpha=255):
+    m_red{red}, m_green{green}, m_blue{blue}, m_alpha{alpha}
+    {
+
+    }
+	void print()
+	{
+		std::cout << "r=" << static_cast<int>(m_red) <<
+			" g=" << static_cast<int>(m_green) <<
+			" b=" << static_cast<int>(m_blue) <<
+			" a=" << static_cast<int>(m_alpha) << '\n';
+	}
+};
+
+void contex()
 {
     Initializer init;
     init.print();
+}
+
+void question()
+{
+    RGBA teal{ 0, 127, 127 };
+	teal.print();
+}
+
+
+int main()
+{
+    contex();
+    question();
+
     return 0;
 }
--- chapter_11/11_7.cpp
+++ chapter_11/11_7.cpp
@@ -38,12 +38,55 @@ public:
 
 };
 
-int main()
+
+class Ball
+{
+private:
+	std::string m_color{"black"};
+	double m_radius{10.0};
+ 
+public:
+
+	Ball(double radius):m_radius{radius}
+	{
+	}
+ 
+	Ball(const std::string &color="black", double radius= 10.0):m_color{color}, m_radius{radius}
+	{
+	}
+ 
+	void print()
+	{
+		std::cout << "color: " << m_color << ", radius: " << m_radius << '\n';
+	}
+};
+
+void contex()
 {
     Rectangle x{ 2.0, 3.0 };
     x.print();
  
     Rectangle y{ 4.0 };
     y.print();
+}
+
+void question()
+{
+    Ball def;
+	def.print();
+ 
+	Ball blue{ "blue" };
+	blue.print();
+	
+	Ball twenty{ 20.0 };
+	twenty.print();
+	
+	Ball blueTwenty{ "blue", 20.0 };
+	blueTwenty.print();
+}
+int main()
+{
+    contex();
+    question();
     return 0;
 }
