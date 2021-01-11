955f4c9ba595266dbb842abfe2416751c44f1fee
mt <mehrdad.tamiji@gmail.com>
2021-01-11 10:42:15 +0330

    files 11_3.cpp and 11_3_quiz.cpp have merged

--- chapter_11/11_3.cpp
+++ chapter_11/11_3.cpp
@@ -43,7 +43,132 @@ public:
     }
 };
 
-int main(int argc, char *argv[])
+class Point_3d
+{
+private:
+    int m_x{};
+    int m_y{};
+    int m_z{};
+
+public:
+    void set_values(int x, int y, int z)
+    {
+        m_x = x;
+        m_y = y;
+        m_z = z;
+    }
+    void print()
+    {
+        std::cout << '<'<<  m_x << ", "<<  m_y << ", "<< m_z << '>' <<  '\n';
+    }
+    bool is_equal(const Point_3d &point)
+    {
+        return (point.m_x == m_x && point.m_y== m_y && point.m_z==m_z);
+    }
+};
+
+class Stack
+{
+private:
+    int stack_top{0};
+    const int length{10};
+    int array[10];
+
+public:
+    void reset()
+    {
+        stack_top = 0;
+    }
+
+    bool push(int a)
+    {
+        if (stack_top >= length)
+            return false;
+        else
+        {
+            array[stack_top] = a;
+            ++stack_top;
+            return true;
+        }
+    }
+
+    int pop()
+    {
+        assert(stack_top!=0);
+        return (array[stack_top--]);
+    }
+
+    void print()
+    {
+        std::cout << '(';
+        for(int i{0}; i < stack_top; ++i)
+            std::cout << array[i] << ' ';
+        std::cout << ')' << '\n';
+    }
+};
+
+void question_2()
+{
+    Point_3d point1 {};
+    point1.set_values(1, 2, 3);
+    point1.print();
+    Point_3d point2{};
+    point2.set_values(1, 2, 3);
+    if (point1.is_equal(point2))
+    {
+        std::cout << "point1 and point2 are equal\n";
+    }
+    else
+    {
+        std::cout << "point1 and point2 are not equal\n";
+    }
+ 
+    Point_3d point3{};
+    point3.set_values(3, 4, 5);
+ 
+    if (point1.is_equal(point3))
+    {
+        std::cout << "point1 and point3 are equal\n";
+    }
+    else
+    {
+        std::cout << "point1 and point3 are not equal\n";
+    }
+}
+
+void question_3()
+{
+	Stack stack;
+	stack.reset();
+ 
+	stack.print();
+ 
+	stack.push(5);
+	stack.push(3);
+	stack.push(8);
+    stack.push(5);
+	stack.push(3);
+	stack.push(8);
+    stack.push(5);
+	stack.push(3);
+	stack.push(8);
+    stack.push(11);
+	stack.push(3);
+	stack.push(8);
+	stack.print();
+ 
+	stack.pop();
+	stack.print();
+ 
+	stack.pop();
+	stack.pop();
+    stack.pop();
+ 
+	stack.print();
+}
+
+
+void contex()
 {
     Date_class today{};
     today.date_set(2020,12,5);
@@ -51,5 +176,11 @@ int main(int argc, char *argv[])
     Date_class another_today{};
     another_today.copy_from(today);
     another_today.print();
+}
+int main(int argc, char *argv[])
+{
+    contex();
+    question_2();
+    question_3();
     return 0;
 }
