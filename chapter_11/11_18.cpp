f1f4727de5cc7d80d03fa8b1fa6f4dd2cf86f23c
mt <mehrdad.tamiji@gmail.com>
2021-01-19 10:54:08 +0330

    file is edited

--- chapter_11/11_18.cpp
+++ chapter_11/11_18.cpp
@@ -11,14 +11,12 @@ class Timer
 {
 private:
     using  clock_t = std::chrono::high_resolution_clock;
-    using second_t = std::chrono::duration<double, std::ratio<1> >;
     std::vector<std::chrono::time_point<clock_t>> m_time_vec{};
 public:
     Timer()
     {
         m_time_vec.push_back(clock_t::now());
     }
-    // void reset(){m_start = clock_t::now();}
     void interval() 
     {
         m_time_vec.push_back(clock_t::now());
@@ -32,21 +30,17 @@ public:
             std::cout << "interval "<< (i) << ": " << std::chrono::duration_cast<std::chrono::milliseconds>(m_time_vec[i] - m_time_vec[i-1]).count() << " milliseconds."  << '\n';
             if(i == (m_time_vec.size() -1))
                 std::cout << "total time: " << std::chrono::duration_cast<std::chrono::milliseconds>(m_time_vec[i] - m_time_vec[0]).count() << " milliseconds." << '\n';
-
         }     
     }
 };
-// (m_time_vec[0].__d.__r % 100000000000) / 10000000
 
 int main()
 {
     Timer t;
     std::array<int, 1000000> array;
-    // fill the array with 10000 to 1
     std::iota(array.rbegin(), array.rend(), 1);
     t.interval();
-        std::array<int, 1000000> array1;
-    // fill the array with 10000 to 1
+    std::array<int, 1000000> array1;
     std::iota(array1.rbegin(), array.rend(), 1);
     std::sort(array.begin(), array.end());
     t.interval();
@@ -58,14 +52,9 @@ int main()
     t.interval();
     std::shuffle(array.begin(), array.end(),g);
     t.interval();
-    
-
-  
- 
     //   std::ranges::sort(array); // Since C++20
     // If your compiler isn't C++20-capable
     std::sort(array.begin(), array.end());
- 
     t.elapsed();
  
   return 0;
